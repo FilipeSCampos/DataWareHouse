727865ef0348565ea64730864b8f1dc04002a641
Filipe Sampaio Campos <fcampos@cgee.org.br>
2023-12-05 16:58:59 -0300

    go

--- airflow/dags/etl.py
+++ airflow/dags/etl.py
@@ -1,74 +1,56 @@
 from airflow import DAG
-from airflow.operators import PythonOperator
-from airflow.providers.google.transfers.http_to_drive import HttpToDriveOperator
-from airflow.providers.google.transfers.drive_to_s3 import DriveToS3Operator
-from airflow.providers.postgres.transfers.postgres_to_postgres import PostgresToPostgresOperator
+from airflow.decorators import dag, task
+from airflow.operators.python import PythonOperator
+from airflow.providers.microsoft.azure.hooks.wasb import WasbHook
 from datetime import datetime, timedelta
-import json
 import pandas as pd
+from sqlalchemy import create_engine
 
-# Define a DAG
+
+# Define the Azure Blob Storage connection ID
+AZURE_CONN_ID = 'https://airflowmineracao.blob.core.windows.net/airlfowdag/dados.json'
+
+# Define the PostgreSQL connection ID
+POSTGRES_CONN_ID = 'localhost:5432'
+
+# Define the DAG
 default_args = {
-    'owner': 'airflow',
+    'owner': 'Filipe Campos',
     'depends_on_past': False,
     'start_date': datetime(2023, 1, 1),
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
 
-dag = DAG(
-    'etl_pipeline',
+@dag(
+    'etl_azure',
     default_args=default_args,
-    description='ETL pipeline for data from Google Drive to PostgreSQL',
+    description='ETL pipeline for data from AzureBlob to Data Warehouse',
     schedule_interval=timedelta(days=1),
 )
 
-# Define a PythonOperator to load data from Google Drive to a JSON file
-def load_data_from_drive(**kwargs):
-    # Use the HttpToDriveOperator to download data from Google Drive
-    task_instance = kwargs['task_instance']
-    task_instance.xcom_push(key='file_id', value='YOUR_GOOGLE_DRIVE_FILE_ID')
+def etl_pipeline():
+    
+    @task()
+    def download_data_from_azure_blob():
 
-    # Load the JSON data from the downloaded file
-    with open('/path/to/downloaded/file.json') as f:
-        data = json.load(f)
+        blob_connection = WasbHook(wasb_conn_id="azure_blob_storage")
+        data = blob_connection.download('airlfowdag','dados.json')
+        return data
 
-    task_instance.xcom_push(key='data', value=data)
+        
 
-load_from_drive = PythonOperator(
-    task_id='load_from_drive',
-    python_callable=load_data_from_drive,
-    provide_context=True,
-    dag=dag,
-)
+    @task()
+    def load_data_to_postgres(data):
+        engine = create_engine('postgresql+psycopg2://postgres:admin@localhost:5432/Techno')
 
-# Define a PythonOperator to transform the data
-def transform_data(**kwargs):
-    task_instance = kwargs['task_instance']
-    data = task_instance.xcom_pull(task_ids='load_from_drive', key='data')
+        for table_name, table_data in data.items():
+            df = pd.DataFrame(table_data)
+            df.to_sql(table_name, con=engine, if_exists='replace', index=False)
 
-    # Perform transformations as needed
-    # ...
+    data = download_data_from_azure_blob()
+    load_data_to_postgres(data)
 
-    # Example: Convert data to Pandas DataFrame
-    df = pd.DataFrame(data['Departamentos'])
+etl_dag = etl_pipeline()
 
-    task_instance.xcom_push(key='transformed_data', value=df)
 
-transform_data = PythonOperator(
-    task_id='transform_data',
-    python_callable=transform_data,
-    provide_context=True,
-    dag=dag,
-)
-
-# Define a PostgresToPostgresOperator to load data into PostgreSQL
-load_to_postgres = PostgresToPostgresOperator(
-    task_id='load_to_postgres',
-    sql="INSERT INTO your_table (your_columns) VALUES (%s, %s, ...)",
-    postgres_conn_id='your_postgres_connection_id',
-    dag=dag,
-)
-
-# Set up task dependencies
-load_from_drive >> transform_data >> load_to_postgres
